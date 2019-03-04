833e6d41b38cb2f20bece3e1810e64dc71cc4edb
kenandting <moon556519@gmail.com>
2019-03-04 11:59:54 +0800

    新增了學生管理套件與基本範例

--- Lecture01-Examples/Example01/Program.cs
+++ Lecture01-Examples/Example01/Program.cs
@@ -10,6 +10,9 @@ namespace Example01
     {
         private static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+            Console.Write("請輸入帳號:");
+            Console.WriteLine("我今天心情很{0}{1}", "開心", "才怪");
         }
     }
 }
---
+++ Lecture01-Examples/Example02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example02
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Sutdent aStudent = new Sutdent()
+            {
+                name = "侯建德",
+                phone = "0958839890",
+                address = "龍華資管"
+            };
+            Console.WriteLine(
+                "學生姓名:{0},電話:{1},地址:{2}"
+                , aStudent.name
+                , aStudent.phone
+                , aStudent.address
+                );
+        }
+    }
+}
